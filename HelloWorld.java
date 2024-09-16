dc7c5ce68dd9f3fcc31299a65cb5dab8e83504e1
Tran-Quoc-Anh-Dung <anhdung200090@gmail.com>
2024-09-16 10:35:07 +0700

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,29 @@
+public class HelloWorld
+{
+public static void main(String[] args)
+{
+    System.out.println("Hello World!");
+
+
+// \t tab
+    System.out.println("Chao mung tan sinh vien nam 2 \t Hoc phan java");
+    
+// \n qua dong moi
+    System.out.println("Khoa Ky Thuat Cong Nghe \n Truong Dai Hoc Hung Vuong TPHCM");
+
+// \" dau ngoac kep
+    System.out.println("\"Trung nghia - Tu Tin - Trach nhiem \"");
+
+// \\ dau ngoac cheo nguoc
+    System.out.println("Chung toi la sinh vien \\ Khoa 2023");
+
+// dung lenh print
+    System.out.print("Chung toi luon tu hao");
+    System.out.print("la nhung sinh vien cham chi nhat \n");
+    System.out.println("");
+
+// dung lenh println
+    System.out.println("Chung toi luon tu hao");
+    System.out.println("la nhung sinh vien cham chi nhat");
+}
+}

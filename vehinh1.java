d870e3896f9a2baeb56adfc021218d618451764e
Tran-Quoc-Anh-Dung <anhdung200090@gmail.com>
2024-09-16 10:38:33 +0700

    Create vehinh1.java

---
+++ vehinh1.java
@@ -0,0 +1,43 @@
+public class vehinh1{
+    public static void main(String[] args) 
+    {   
+        // hinh 1
+        System.out.println("   /\\");
+        System.out.println("  /  \\");
+        System.out.println(" /    \\");
+        System.out.println(" \\    /");
+        System.out.println("  \\  /");
+        System.out.println("   \\/");
+        System.out.println();
+
+        //hinh 2
+        System.out.println("\\    /");
+        System.out.println(" \\  /");
+        System.out.println("  \\/");
+        System.out.println("  /\\");
+        System.out.println(" /  \\");
+        System.out.println("/    \\");
+        System.out.println();
+
+        //hinh 3
+        System.out.println("   /\\");
+        System.out.println("  /  \\");
+        System.out.println(" /    \\");
+        System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
+        System.out.println("|  Viet|");
+        System.out.println("|   Nam|");
+        System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
+        System.out.println("   /\\");
+        System.out.println("  /  \\");
+        System.out.println(" /    \\");
+        System.out.println();
+
+        
+    }
+}

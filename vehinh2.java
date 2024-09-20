817957238e4818f1a9c8d25e8c73864c57f00250
Tran-Quoc-Anh-Dung <anhdung200090@gmail.com>
2024-09-20 15:49:27 +0700

    Add files via upload

---
+++ vehinh2.java
@@ -0,0 +1,17 @@
+public class vehinh2{
+    public static void main(String[] args) {
+        
+        // ve hinh
+        System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
+        System.out.println();
+
+        System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
+
+    }
+}
---
+++ vehinh3.java
@@ -0,0 +1,20 @@
+public class vehinh3{
+    public static void main(String[] args) {
+
+        drawBox();
+        System.out.println();
+        drawBox();
+    }
+
+    public static void drawBox() {
+        
+
+        
+        //ve hinh
+        System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
+        System.out.println();
+    }
+}

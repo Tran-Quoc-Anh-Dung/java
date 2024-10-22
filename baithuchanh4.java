2da44b909c53388b451ac199ec6f68d48911c9dc
Tran-Quoc-Anh-Dung <anhdung200090@gmail.com>
2024-10-22 09:16:41 +0700

    java

--- baithuchanh4.java
+++
@@ -1,8 +0,0 @@
-public class baithuchanh4{
-    public static void main(String[] args) {
-        for(int i=1; i<10; i++)
-        {
-            System.out.println(i+ " Binh phuong la: " + (i*i));
-        }
-    }
-}
